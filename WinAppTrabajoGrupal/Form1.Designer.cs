bb4b4e0fb87d63c4b43871dd217fd9ef9280a3cc
Comtex765 <ferchon123443@gmail.com>
2023-05-09 20:32:10 -0500

    este este sí

--- WinAppTrabajoGrupal/Form1.Designer.cs
+++ WinAppTrabajoGrupal/Form1.Designer.cs
@@ -167,7 +167,6 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Aplicación de empresa";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
